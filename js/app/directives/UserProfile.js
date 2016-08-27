5fbcc43b8f4f0db22feaf6ade4fd8f894b63a9e3
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-27 15:55:07 -0400

    Done.

--- js/app/directives/UserProfile.js
+++ js/app/directives/UserProfile.js
@@ -1,15 +1,18 @@
 function UserProfile() {
-	return {
-		transclude: true,
-		template: [
-			'<div>',
-				'<h3>User Profile</h3>',
-				'<span></span>',
-			'</div>'
-		].join('')
-	}
+  return {
+    transclude: true,
+    template: [
+      '<div>',
+      '<h3>User Profile</h3>',
+      '<span></span>',
+      '</div>'
+    ].join(''),
+    link: function (scope, elem, attrs, ctrl, transclude) {
+      elem.find('span').after(transclude());
+    }
+  }
 }
 
 angular
-	.module('app')
-	.directive('userProfile', UserProfile);
+  .module('app')
+  .directive('userProfile', UserProfile);
